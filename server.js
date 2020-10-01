6a20e66480378d8f2c9927f52ee30e5995e36da2
Eduardo Pineda <edibozu@gmail.com>
2020-10-01 19:11:46 -0400

    require fs and included middleware

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = requre("express");
+const fs= require("fs");
 const app = express();
 
 const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ edtended: true}));
+app.use(express.json());
 
