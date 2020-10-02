8c45f2395f866e3609c72219c075e8460d29751d
Eduardo Pineda <edibozu@gmail.com>
2020-10-02 15:50:30 -0400

    cleaned up server.js and working on app.post on api.routes file

--- routing/api-routes.js
+++ routing/api-routes.js
@@ -5,5 +5,16 @@ module.exports = (app) => {
     app.get("/api/notes", (req, res) => {
         return res.sendFile(path.join(__dirname, "../db/db.json"));
     });
+
+    app.post("/api/notes", (req, res) => {
+        fs.readFile("../db/db.json", "utf-8", (err, data) => {
+            if(err){
+                return res.send("Couldn't retrieve information!");
+                db.push(req.body);
+            }
+
+        } );
+        
+    })
     
 }
--- server.js
+++ server.js
@@ -2,13 +2,14 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
+const db = require("./db/db.json");
 const app = express();
 
 // The port that the server will be listening on.
 const PORT = process.env.PORT || 8080;
 
 // Middleware
-app.use(express.urlencoded({ edtended: true}));
+app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 app.use(express.static("public"));
 
@@ -18,7 +19,7 @@ require("./routing/api-routes.js")(app);
 
 // When the user runs the program, they will be alerted that the server is listening to their request.
 app.listen(PORT, () => {
-    console.log("App listening on PORT: " + PORT);
+    console.log("App listening on PORT: http://localhost" + PORT);
 });
 
 
