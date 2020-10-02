f921d8d7e0dda56db8d9045881ac0c7e14aa7ba2
Eduardo Pineda <edibozu@gmail.com>
2020-10-01 21:44:25 -0400

    require path and also added express.static to middleware

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = requre("express");
 const fs= require("fs");
+const path = require("path");
 const app = express();
 
 const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ edtended: true}));
 app.use(express.json());
+app.use(express.static("public"));
+
+
 
