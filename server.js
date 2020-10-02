8d58b721426c7978461f25717d94dbac22dd125a
Eduardo Pineda <edibozu@gmail.com>
2020-10-01 23:17:01 -0400

    created a folder titled 'routing' which will contain the api and html routes. instead of placing all the code on one page, it will be neatly organized in their own seperate files inside a seperate folder.

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
-const express = requre("express");
-const fs= require("fs");
+// Dependencies needed for this application.
+const express = require("express");
+const fs = require("fs");
 const path = require("path");
 const app = express();
 
+// The port that the server will be listening on.
 const PORT = process.env.PORT || 8080;
 
+// Middleware
 app.use(express.urlencoded({ edtended: true}));
 app.use(express.json());
 app.use(express.static("public"));
 
+require("./routing/html-routes.js")(app);
+
+app.get("/api/notes", (req, res) => {
+    return res.sendFile(path.join(__dirname, "/db/db.json"));
+});
 
 
